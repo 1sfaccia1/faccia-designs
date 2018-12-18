af2c48cf3e4595feb08ce8fc9e3386d9ea4a4861
1stfaccia1 <chad.m.wittman75@gmail.com>
2018-12-18 02:28:32 -0600

    last 1 tonight night night

--- js/main.js
+++ js/main.js
@@ -4,5 +4,6 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $('.fixed-action-btn').floatingActionButton();
+    $('.scrollspy').scrollSpy();
   }); // end of document ready
 })(jQuery);
