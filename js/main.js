725d40d4600ea9dddf02cdfd2d9882adf9786618
1stfaccia1 <chad.m.wittman75@gmail.com>
2019-02-17 17:51:37 -0600

    add

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
 (function($){
   $(function(){
 
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
-    $('.fixed-action-btn').floatingActionButton();
+    TweenMax.from('.mainHeader', 1, { y:-50, autoAlpha: 0, ease: Power1.easeOut });
+
+      // var header = $("#header"),
+      //     logo-container = $("#logo-container"),
+      //
+      //     main-logo = $("#main-logo"),
+      //
+      //
+      //     tl = new timeLineLite({paused:true});
+      //
+      // tl
+      //
+      //   .from(main-logo, 0.3, {opacity: 0;});
+
+
+
+
+      $('.sidenav').sidenav();
+      $('.parallax').parallax();
+      $('.fixed-action-btn').floatingActionButton();
+
   }); // end of document ready
   $('.scrollspy').scrollSpy();
 })(jQuery);
